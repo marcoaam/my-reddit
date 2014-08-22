b50f89b9e7e3fbe7a0a3de876ead02ec0010e30b
marcoaam <marco.army@gmail.com>
2014-08-22 11:58:50 +0100

    shows all posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -7,6 +7,16 @@ describe 'Posts' do
 			visit '/posts'
 			expect(page).to have_content 'No posts have been added'
 		end
-
 	end
+
+	context 'With posts' do
+		before(:each) do
+			Post.create(text: 'Challenge day at makers academy')
+		end
+
+		it 'Shows all posts' do
+			visit '/posts'
+			expect(page).to have_content 'Challenge day at makers academy'
+		end
+	end 
 end
