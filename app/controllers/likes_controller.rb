c77a65402035b478e2efa37f6517f8ec2a2341c9
marcoaam <marco.army@gmail.com>
2014-08-26 09:33:17 +0100

    simplify complex method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,22 +6,20 @@ class LikesController < ApplicationController
 		post = Post.find(params[:post_id])
 		if !find_post_dislike(post, current_user.id).nil?
 			find_post_dislike(post, current_user.id).destroy
-			redirect_to '/posts'
 		else
 			post.likes.find_or_create_by(user_id: current_user.id)
-			redirect_to '/posts'
 		end
+		redirect_to '/posts'
 	end
 
 	def destroy
 		post = Post.find(params[:post_id])
 		if !post.likes.find_by(user_id: current_user.id).nil?
 			post.likes.find_by(user_id: current_user.id).destroy
-			redirect_to '/posts'
 		else
 			post.dislikes.find_or_create_by(user_id: current_user.id)
-			redirect_to '/posts'
 		end
+		redirect_to '/posts'
 	end
 
 	def find_post_dislike(post, user_id)
